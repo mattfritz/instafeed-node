f4880762bed1c5f131f5d40221ed48f5c5daedf2
Matt Fritz <mfritzer@gmail.com>
2014-08-10 12:30:13 -0600

    Renamed app file.

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
-var express = require('express');
-var app = express();
+// Packages
+var express     = require('express'),
+    app         = express(),
+    bodyParser  = require('body-parser'),
+    mongoose    = require('mongoose'),
+    instagram   = require('instagram-node').instagram();
+
+// Configuration
+var port = process.env.PORT || 8080; // App port
+
+mongoose.connect('mongodb://localhost:27017');
+
+app.use(bodyParser());
+
+// Routes
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  res.json({ message: 'test json' });
+});
+
+router.get('/html', function(req, res) {
+  res.send('<h1>Test HTML</h1>');
+});
+
+app.use(router);
+
+// Server
+app.listen(port);
+console.log('Server is running at http://localhost:' + port);
