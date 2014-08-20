fa88b15b7a2df1cadd30d954363a196cd53b9513
Matt Fritz <mfritzer@gmail.com>
2014-08-20 16:48:56 -0600

    Added coverage task and refactored test runner.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,34 @@
-var gulp  = require('gulp'),
-    mocha = require('gulp-mocha'),
-    exit  = require('gulp-exit');
+var gulp     = require('gulp'),
+    mocha    = require('gulp-mocha'),
+    istanbul = require('gulp-istanbul'),
+    exit     = require('gulp-exit');
 
-gulp.task('default', ['test']);
+var sources = {
+  test: ['./test/*.js'],
+  app:  ['app.js']
+};
 
-gulp.task('test', function () {
-  return gulp.src('./test/test.js', {read: false})
+var testRunner = function() {
+  gulp.src('./test/test.js', {read: false})
     .pipe(mocha({
       reporter: 'progress',
       timeout: 2000
     }))
     .pipe(exit());
+};
+
+gulp.task('default', ['test']);
+
+gulp.task('test', function() {
+  return testRunner();
 });
+
+gulp.task('coverage', function() {
+  return gulp.src(sources.app)
+    .pipe(istanbul())
+    .on('finish', function() {
+      testRunner();
+    });
+});
+
+
