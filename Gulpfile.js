4e21402f62d2938cce5bd7f73db4d930c7347651
Matt Fritz <mfritzer@gmail.com>
2014-08-20 16:08:07 -0600

    Added default task.

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,9 @@ var gulp  = require('gulp'),
     mocha = require('gulp-mocha'),
     exit  = require('gulp-exit');
 
-gulp.task('default', function () {
+gulp.task('default', ['test']);
+
+gulp.task('test', function () {
   return gulp.src('./test/test.js', {read: false})
     .pipe(mocha({
       reporter: 'progress',
