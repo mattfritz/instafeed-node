1a1093e097f9f9a00fcad83cfd13424880c9f3eb
Matt Fritz <mfritzer@gmail.com>
2014-08-21 15:55:33 -0600

    Added test for challenge response.

--- test/test.js
+++ test/test.js
@@ -9,7 +9,17 @@ describe('App', function() {
     it('responds with HTML', function(done) {
       request(app)
       .get('/')
-      .expect(200, done);
+      .expect(200)
+      .expect('<h1>THIS IS WORKING</h1>', done)
+    });
+  });
+
+  describe('GET /tags/yolo', function() {
+    it('responds with the challenge key', function(done) {
+      request(app)
+        .get('/tags/yolo')
+        .query('hub.challenge=a1b2c3d4')
+        .expect('a1b2c3d4', done);
     });
   });
 
